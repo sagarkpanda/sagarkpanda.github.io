99b6733bca60d2befc4cf584c02f3b105be8c96e
Sagar <skpanda256@gmail.com>
2023-10-02 20:08:40 +0530

    new website

---
+++ src/portfolio.js
@@ -0,0 +1,455 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Sagar Panda",
+  description:
+    "A passionate Cloud DevOps Engineer.",
+  og: {
+    title: "Sagar's Portfolio",
+    type: "website",
+    url: "https://sagarkrp.github.io",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Sagar Panda",
+  logo_name: "Sagar Panda",
+  nickname: "",
+  subTitle:
+    "A passionate Software Engineer focused on Cloud and DevOps automation",
+
+  // portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+  githubProfile: "https://github.com/sagarkrp",
+};
+
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/sagarkrp",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/sagarkrp/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+
+  {
+    name: "Gmail",
+    link: "mailto:no-reply@accounts.google.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+
+];
+
+const skills = {
+  data: [
+    {
+      title: "DevOps and Cloud Infra",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "logos:aws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Jenkins",
+          fontAwesomeClassname: "devicon:jenkins",
+          style: {
+            backgroundColor: "transparent",
+            // color: "#D00000",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "Kubernetes",
+          fontAwesomeClassname: "simple-icons:kubernetes",
+          style: {
+            color: "#326CE5",
+          },
+        },
+      ],
+    },
+    {
+      title: "Software Development and Scripting",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Developing scritpts for automation",
+        "⚡ Scripting with Pyhton and Bash",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "logos:python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Bash",
+          fontAwesomeClassname: "devicon:bash",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+
+      ],
+    },
+
+  ],
+};
+
+
+const degrees = {
+  degrees: [
+    {
+      title: "Biju Patnaik University of Technology",
+      subtitle: "Master of Computer Applications (MCA)",
+      logo_path: "bput.png",
+      alt_name: "BPUT",
+      duration: "2016 - 2018",
+      descriptions: [
+        "⚡ Compuetr Science and Computer Application",
+      ],
+      website_link: "https://www.bput.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "DevOps for Beginners",
+      subtitle: "DevOps",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-4c092504-0e30-4b47-b196-ca772cf8ad5d/",
+      alt_name: "Udemy",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Amazon Web Services",
+      subtitle: "AWS",
+      logo_path: "udemy.png",
+      certificate_link:
+        "https://www.udemy.com/certificate/UC-4d0092fe-d099-4aff-a5a4-c0fefbdd678f/",
+      alt_name: "aws",
+      color_code: "#00000099",
+    },
+
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#F79F1B",
+      },
+      profileLink: "",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      work: true,
+      experiences: [
+        {
+          title: "Associate Software Engineer",
+          company: "Valeo",
+          company_url: "https://valeo.com/",
+          logo_path: "Valeo.png",
+          duration: "June 2018 - Present",
+          location: "Chennai, India",
+          description:
+            "Working on CI/CD Platform autoamtion for automotive solutions. Tools and Technologies in use are AWS, Jenkins, Ansible, Python, Nexus, Clockwork SAST. Other Tools and technologies I have used: Docker, Kubernetes, Terraform, Linux, Git, SonarQube, Linux, Apache & NGINX webservers, Prometheus, Grafana, New Relci etc",
+          color: "#0879bf",
+        },
+
+      ],
+    },
+    // {
+    //   title: "Internships",
+    //   experiences: [
+    //     {
+    //       title: "Machine Learning Intern",
+    //       company: "TikTok Inc.",
+    //       company_url: "https://www.tiktok.com/en/",
+    //       logo_path: "tiktok_logo.png",
+    //       duration: "May 2022 - Aug 2022",
+    //       location: "San Francisco, USA",
+    //       description:
+    //         "Building new features on the backend recommendation system, specifically ranking algorithms for Ads that touch hundreds of millions of people around the world. Improving online and offline content ranking algorithms by performing hard sample data replays for training steps.",
+    //       color: "#000000",
+    //     },
+    //     {
+    //       title: "Data Science Research Intern",
+    //       company: "Delhivery Pvt. Ltd.",
+    //       company_url: "https://www.delhivery.com/",
+    //       logo_path: "delhivery_logo.png",
+    //       duration: "May 2019 - Sept 2019",
+    //       location: "Gurgaon, Haryana",
+    //       description:
+    //         "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+    //       color: "#ee3c26",
+    //     },
+    //     {
+    //       title: "Data Science Intern",
+    //       company: "Intel Indexer LLC",
+    //       company_url:
+    //         "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
+    //       logo_path: "intel_logo.jpg",
+    //       duration: "Nov 2018 - Dec 2018",
+    //       location: "Work From Home",
+    //       description:
+    //         "This is financial Solution Company. I have made Supervised Learning model for the company which can perform time series analysis on Stock price data for 32 companies. I have built LSTM Neural Networks Model and trained the data of 32 companies for last 2 years. This model is also used for forecasting.",
+    //       color: "#0071C5",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Volunteerships",
+    //   experiences: [
+    //     {
+    //       title: "Google Explore ML Facilitator",
+    //       company: "Google",
+    //       company_url: "https://about.google/",
+    //       logo_path: "google_logo.png",
+    //       duration: "June 2019 - April 2020",
+    //       location: "Hyderabad, Telangana",
+    //       description:
+    //         "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+    //       color: "#4285F4",
+    //     },
+    //     {
+    //       title: "Microsoft Student Partner",
+    //       company: "Microsoft",
+    //       company_url: "https://www.microsoft.com/",
+    //       logo_path: "microsoft_logo.png",
+    //       duration: "Aug 2019 - May 2020",
+    //       location: "Hyderabad, Telangana",
+    //       description:
+    //         "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
+    //       color: "#D83B01",
+    //     },
+    //     {
+    //       title: "Mozilla Campus Captain",
+    //       company: "Mozilla",
+    //       company_url: "https://www.mozilla.org/",
+    //       logo_path: "mozilla_logo.png",
+    //       duration: "Oct 2019 - May 2020",
+    //       location: "Kurnool, Andhra Pradesh",
+    //       description:
+    //         "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
+    //       color: "#000000",
+    //     },
+    //     {
+    //       title: "Developer Students Club Member",
+    //       company: "DSC IIITDM Kurnool",
+    //       company_url:
+    //         "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
+    //       logo_path: "dsc_logo.png",
+    //       duration: "Jan 2018 - May 2020",
+    //       location: "Kurnool, Andhra Pradesh",
+    //       description:
+    //         "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
+    //       color: "#0C9D58",
+    //     },
+    //     {
+    //       title: "Developer Program Member",
+    //       company: "Github",
+    //       company_url: "https://github.com/",
+    //       logo_path: "github_logo.png",
+    //       duration: "July 2019 - PRESENT",
+    //       location: "Work From Home",
+    //       description:
+    //         "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
+    //       color: "#181717",
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "",
+  avatar_image_path: "projects_image.svg",
+};
+
+// const publicationsHeader = {
+//   title: "Publications",
+//   description:
+//     "",
+//   avatar_image_path: "projects_image.svg",
+// };
+
+// const publications = {
+//   data: [
+//     {
+//       id: "",
+//       name: "",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzi=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNze=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzt=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzb=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//   ],
+// };
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "panda.jpg",
+    description:
+      "I am available on Linkedin. You can message me, I will try to reply within 24 hours.",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "For individual fundamental empowerment, I do a lot of experminet on the filed of Cloud and DevOps that I share on Medium.com so other entusiasts can experience the same.",
+    link: "https://sagarkrp.medium.com/",
+    avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle:
+      "",
+    locality: "",
+    country: "IN",
+    region: "",
+    postalCode: "",
+    streetAddress: "",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "",
+  },
+  phoneSection: {
+    title: "",
+    subtitle: "",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  // publicationsHeader,
+  // publications,
+  contactPageData,
+};
